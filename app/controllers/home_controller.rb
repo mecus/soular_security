46c70adda3d116c54daeee47e24896af3a848733
mecus <design@miscotech.co.uk>
2016-08-19 12:21:41 +0100

    changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,9 @@
 class HomeController < ApplicationController
   def index
-  	@posts = Post.all.paginate(:page => params[:page], :per_page => 4).order('created_at DESC')
+  	@posts = Post.all.paginate(:page => params[:page], :per_page => 6).order('created_at DESC')
   end
   def about
+  	@posts = Post.all.paginate(:page => params[:page], :per_page => 4).order('created_at DESC')
   end
   def contact
   end
--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -2,36 +2,47 @@ class ServicesController < ApplicationController
   def index
   end
   def security_guard
+    @posts = Post.all.paginate(:page => params[:page], :per_page => 4).order('created_at DESC')
   	@title = "Security Guards Services"
   end
   def close_protection
+    @posts = Post.all.paginate(:page => params[:page], :per_page => 4).order('created_at DESC')
     @title = "Close Protection Services"
   end
   def event_security
+    @posts = Post.all.paginate(:page => params[:page], :per_page => 4).order('created_at DESC')
     @title = "Event Security Services"
   end
   def contruction_security
+    @posts = Post.all.paginate(:page => params[:page], :per_page => 4).order('created_at DESC')
     @title = "Construction Security Services"
   end
   def hospitality_security
+    @posts = Post.all.paginate(:page => params[:page], :per_page => 4).order('created_at DESC')
     @title = "Hospitality Security Services"
   end
   def retail_security
+    @posts = Post.all.paginate(:page => params[:page], :per_page => 4).order('created_at DESC')
     @title = "Retail Security Services"
   end
   def concierge_security
+    @posts = Post.all.paginate(:page => params[:page], :per_page => 4).order('created_at DESC')
     @title = "Concierge Security Services"
   end
   def corporate_security
+    @posts = Post.all.paginate(:page => params[:page], :per_page => 4).order('created_at DESC')
     @title = "Corporate Security Services"
   end
   def hotel_security
+    @posts = Post.all.paginate(:page => params[:page], :per_page => 4).order('created_at DESC')
     @title = "Hotel Security Services"
   end
   def mobile_security
+    @posts = Post.all.paginate(:page => params[:page], :per_page => 4).order('created_at DESC')
     @title = "Mobile Security Services"
   end
   def industrial_security
+    @posts = Post.all.paginate(:page => params[:page], :per_page => 4).order('created_at DESC')
     @title = "Industrial Security Services"
   end
 end
